cb3d56cb11b9b5c33e8451625f7e7602514675df
rsuregar <rsuregar@gmail.com>
2022-02-17 16:38:23 +0800

    update

--- app/Services/BadgeClassService.php
+++ app/Services/BadgeClassService.php
@@ -22,20 +22,20 @@ class BadgeClassService
             'description' => $badgeClass->description,
             'image' => $badgeClass->image,
             'criteria' => "https://rsuregar.my.id",
-            'issuer' => route('issuer.show', $badgeClass->issuer_uuid),
-            'tags' => $badgeClass->tags,
-            'alignment' => $badgeClass->alignment,
-            'issuance' => [
-                'type' => $badgeClass->issuance_type,
-                'count' => $badgeClass->issuance_count,
-                'earn' => $badgeClass->issuance_earn,
-                'revoked' => $badgeClass->issuance_revoked,
-            ],
-            'related' => [
-                'badge' => $badgeClass->related_badge,
-                'evidence' => $badgeClass->related_evidence,
-            ],
-            'extensions' => $badgeClass->extensions,
+            'issuer' => route('issuer.show', $badgeClass->issuer_uuid)
+            // 'tags' => $badgeClass->tags,
+            // 'alignment' => $badgeClass->alignment,
+            // 'issuance' => [
+            //     'type' => $badgeClass->issuance_type,
+            //     'count' => $badgeClass->issuance_count,
+            //     'earn' => $badgeClass->issuance_earn,
+            //     'revoked' => $badgeClass->issuance_revoked,
+            // ],
+            // 'related' => [
+            //     'badge' => $badgeClass->related_badge,
+            //     'evidence' => $badgeClass->related_evidence,
+            // ],
+            // 'extensions' => $badgeClass->extensions,
         ];
         return response()->json($data, 200);
     }
